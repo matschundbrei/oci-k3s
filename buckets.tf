bd543a7a1d3dd065f5b3baae405d853409ae2209
Jan Kapellen <jkapellen@gmail.com>
2022-11-17 22:52:45 +0100

    buckets cleanup

--- buckets.tf
+++ buckets.tf
@@ -1,22 +1,16 @@
 locals {
-  bucket_names = [
-    "solutr-contrib",
-    "solutr-images",
-    "solutr-backups",
-  ]
+  s3_compat_api_endpoint = "${data.oci_objectstorage_namespace.this.namespace}.compat.objectstorage.${var.region}.oraclecloud.com"
 }
 
 data "oci_objectstorage_namespace" "this" {
   compartment_id = var.compartment_id
 }
 
-
-resource "oci_objectstorage_bucket" "solutr" {
-  count          = length(local.bucket_names)
-  name           = local.bucket_names[count.index]
+resource "oci_objectstorage_bucket" "etcd_backup" {
+  name           = "etcd-backups"
   namespace      = data.oci_objectstorage_namespace.this.namespace
   compartment_id = var.compartment_id
   access_type    = "NoPublicAccess"
   auto_tiering   = "InfrequentAccess"
-  versioning     = "Enabled"
+  versioning     = "Disabled"
 }

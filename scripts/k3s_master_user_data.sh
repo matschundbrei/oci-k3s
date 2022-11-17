65a2e85cf8be9de2cb9f5e323faec4c9aa3afef8
Jan Kapellen <jkapellen@gmail.com>
2022-11-17 23:09:06 +0100

    adding in the config to main node user data

--- scripts/k3s_master_user_data.sh
+++ scripts/k3s_master_user_data.sh
@@ -9,7 +9,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 # install fun tools
-sudo apt-get -y install jq 
+sudo apt-get -y install jq
 
 export NODE_IP4=$(curl -sSL -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/vnics/ | jq -r '.[].privateIp')
 export CLUSTER_CIDR=$(curl -sSL -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq -r '.metadata.k3s_cluster_cidr')
@@ -44,7 +44,18 @@ sudo -E iptables-save | sudo tee /etc/iptables/rules.v4 > /dev/null
 sudo -E ip6tables-save | sudo tee /etc/iptables/rules.v6 > /dev/null
 
 
+export S3_ENDPOINT=$(curl -sSL -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq -r '.metadata.s3_endpoint_url')
+export S3_ACCESS_KEY=$(curl -sSL -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq -r '.metadata.s3_access_key')
+export S3_SECRET_KEY=$(curl -sSL -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq -r '.metadata.s3_secret_key')
+export S3_BUCKET=$(curl -sSL -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq -r '.metadata.s3_bucket_name')
 export K3S_TOKEN=$(curl -sSL -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq -r '.metadata.k3s_secret')
 export K3S_CLUSTER_INIT="True"
 
-curl -sfL https://get.k3s.io | sudo -E sh -s - server --node-ip "${NODE_IP}" --cluster-cidr "${CLUSTER_CIDR}" --service-cidr "${SERVICE_CIDR}"
+curl -sfL https://get.k3s.io | sudo -E sh -s - server \
+  --node-ip "${NODE_IP}" \
+  --cluster-cidr "${CLUSTER_CIDR}" \
+  --service-cidr "${SERVICE_CIDR}" \
+  --etcd-s3 --etcd-s3-endpoint "${S3_ENDPOINT}" \
+  --etcd-s3-access-key "${S3_ACCESS_KEY}" \
+  --etcd-s3-secret-key "${S3_SECRET_KEY}" \
+  --etcd-s3-bucket "${S3_BUCKET}"
